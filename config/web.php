7064754bba1bf6199f02007c5f33572248b92106
2000AndreaPaolo <2000.andrea.paolo@gmail.com>
2020-03-11 18:13:01 +0100

    cookieValidationKey

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'posdpaodj16561515aspdji3j904u932udasasduoqwuoenaw',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
